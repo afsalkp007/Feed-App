0a2f8d6ec9cb732cab6848894cd70fce1740f335
Afsal <afsalkp.mohammed@gmail.com>
2024-06-27 13:36:57 +0400

    Add fade in animation

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,40 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Afsal on 27/06/2024.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+  @IBOutlet private(set) var locationContainer: UIView!
+  @IBOutlet private(set) var locationLabel: UILabel!
+  @IBOutlet private(set) var descriptionLabel: UILabel!
+  @IBOutlet private(set) var feedImageView: UIImageView!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    feedImageView.alpha = 0
+  }
+
+  override func prepareForReuse() {
+    super.prepareForReuse()
+
+    feedImageView.alpha = 0
+  }
+
+  func fadeIn(_ image: UIImage?) {
+    feedImageView.image = image
+
+    UIView.animate(
+      withDuration: 0.3,
+      delay: 0.3,
+      options: [],
+      animations: {
+        self.feedImageView.alpha = 1
+      })
+  }
+
+}
