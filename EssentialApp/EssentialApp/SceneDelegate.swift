24c50bc4f1a4f17d4dd3a8f203cff87a951b5abd
Afsal <afsalkp.mohammed@gmail.com>
2024-07-16 15:55:38 +0400

    Added UI tests

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -11,29 +11,40 @@ import EssentialFeed
 import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  
   var window: UIWindow?
+  
+  let localStoreURL = NSPersistentContainer
+    .defaultDirectoryURL()
+    .appendingPathComponent("feed-store.sqlite")
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let _ = (scene as? UIWindowScene) else { return }
     
-    let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-    
-    let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!
+    let remoteClient = makeRemoteClient()
     let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
     let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
-    
-    let localStoreURL = NSPersistentContainer
-      .defaultDirectoryURL()
-      .appending(path: "feed-store.sqlite")
-    
+
     let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
     let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
     let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-    
+
     window?.rootViewController = FeedUIComposer.feedComposedWith(
-      feedLoader: localFeedLoader,
-      imageLoader: localImageLoader)
+      feedLoader: FeedLoaderWithFallbackComposite(
+        primary: FeedLoaderCacheDecorator(
+          decoratee: remoteFeedLoader,
+          cache: localFeedLoader),
+        fallback: localFeedLoader),
+      imageLoader: FeedImageDataLoaderWithFallbackComposite(
+        primary: localImageLoader,
+        fallback: FeedImageDataLoaderCacheDecorator(
+          decoratee: remoteImageLoader,
+          cache: localImageLoader)))
+
+  }
+  
+  func makeRemoteClient() -> HTTPClient {
+    return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
   }
 }
 
