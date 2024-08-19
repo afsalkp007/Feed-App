4528afb0252a6086d8a0bc01f77f4d4dbc042e9f
Afsal <afsalkp.mohammed@gmail.com>
2024-08-19 17:01:16 +0400

    Added performance optimizations by logging and profiling

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -13,16 +13,23 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
   private let loader: () -> AnyPublisher<Resource, Error>
   var presenter: LoadResourcePresenter<Resource, View>?
   private var cancellable: Cancellable?
+  private var isLoading = false
   
   init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
     self.loader = loader
   }
   
   func loadResource() {
+    guard !isLoading else { return }
+
     presenter?.didStartLoading()
+    isLoading = true
     
     cancellable = loader()
       .dispatchOnMainThread()
+      .handleEvents(receiveCancel: { [weak self] in
+        self?.isLoading = false
+      })
       .sink(
         receiveCompletion: { [weak self] completion in
       switch completion {
@@ -32,6 +39,7 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
         self?.presenter?.didFinishLoading(with: error)
       }
       
+          self?.isLoading = false
     }, receiveValue: { [weak self] resource in
       self?.presenter?.didFinishLoading(with: resource)
     })
--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -55,17 +55,21 @@ class CommentsUIIntegrationTests: XCTestCase {
     XCTAssertEqual(sut.errorViewVisible, false)
   }
   
-  func test_loadCommentsActions_requestFeedFromLoader() {
+  func test_loadCommentsActions_requestCommentsFromLoader() {
     let (sut, loader) = makeSUT()
     XCTAssertEqual(loader.loadCommentsCallCount, 0, "Expected no loading requests before view is loaded")
     
     sut.simulateAppearance()
     XCTAssertEqual(loader.loadCommentsCallCount, 1, "Expected a loading request once view is loaded")
     
+    sut.simulateUserInitiatedReload()
+    XCTAssertEqual(loader.loadCommentsCallCount, 1, "Expected no request until previous completes")
     
+    loader.completeCommentsLoading(at: 0)
     sut.simulateUserInitiatedReload()
     XCTAssertEqual(loader.loadCommentsCallCount, 2, "Expected another loading request once user initiates a reload")
     
+    loader.completeCommentsLoading(at: 1)
     sut.simulateUserInitiatedReload()
     XCTAssertEqual(loader.loadCommentsCallCount, 3, "Expected yet another loading request once user initiates another reload")
   }
@@ -102,7 +106,7 @@ class CommentsUIIntegrationTests: XCTestCase {
     assertThat(sut, isRendering: [comment0, comment1])
   }
   
-  func test_loadCommentsCompletion_rendersSuccessfullyLoadedEmptyCommentsAfterNonEmptyFeed() {
+  func test_loadCommentsCompletion_rendersSuccessfullyLoadedEmptyCommentsAfterNonEmptyComments() {
     let comment = makeComment()
     let (sut, loader) = makeSUT()
     
@@ -203,7 +207,7 @@ class CommentsUIIntegrationTests: XCTestCase {
   }
   
   private class LoaderSpy {
-    // MARK: - FeedLoader
+    // MARK: - CommentsLoader
 
     private var requests = [PassthroughSubject<[ImageComment], Error>]()
 
@@ -219,6 +223,7 @@ class CommentsUIIntegrationTests: XCTestCase {
 
     func completeCommentsLoading(with comments: [ImageComment] = [], at index: Int = 0) {
       requests[index].send(comments)
+      requests[index].send(completion: .finished)
     }
     
     func completeCommentsLoadingWithError(at index: Int = 0) {
